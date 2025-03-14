34afd1cddff40667581bdba86a6d57aa10e31fa4
DarkblooM-IO <darkbloompro@gmail.com>
2025-03-14 10:27:41 +0100

    03/14/25 10:27:41

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env -S python3 -OO
+import argparse, secrets, string
+from pyperclip import copy
+
+def getOps() -> argparse.Namespace:
+  parser = argparse.ArgumentParser(prog="genpass",
+                                   description="CLI random password generator")
+
+  parser.add_argument("-l", "--length",
+                      type=int,
+                      required=False,
+                      default=16,
+                      help="length of password (default 16, minimum 8)")
+  parser.add_argument("-c", "--copy",
+                      action="store_true",
+                      required=False,
+                      help="copy password to clipboard")
+  parser.add_argument("-e", "--exclude",
+                      nargs="+",
+                      required=False,
+                      help="list of characters to exclude from password")
+
+  return parser.parse_args()
+
+def main() -> None:
+  args = getOps()
+  characters = string.ascii_letters + string.digits + string.punctuation
+  length = args.length if args.length >= 8 else 8
+
+  if args.exclude:
+    for c in "".join(args.exclude):
+      characters = characters.replace(c, "")
+
+  password = "".join(secrets.choice(list(characters)) for _ in range(length))
+
+  if args.copy:
+    try:
+      copy(password)
+      print("Password copied to clipboard")
+    except e:
+      print("Couldn't copy password:", e)
+  else:
+    print(password)
+
+if __name__ == "__main__":
+  main()
